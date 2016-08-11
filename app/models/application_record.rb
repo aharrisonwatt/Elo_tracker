5abe0ddee587397610bd455454c54d6ed96fe303
aharrisonwatt <aharrisonwatt@gmail.com>
2016-08-11 16:01:17 -0700

    user model set up

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,23 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  helper_method :current_user, :signed_in?
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def sign_in(user)
+    @current_user = user
+    session[:session_token] = user.reset_token!
+  end
+
+  def sign_out
+    session[:session_token] = nil
+    current_user.try(:reset_token!)
+    @current_user = nil
+  end
+
 end
