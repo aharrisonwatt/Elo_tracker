3e171708a6fc29050b6b5aa5f491a9c3b7b487a6
aharrisonwatt <aharrisonwatt@gmail.com>
2016-08-16 14:00:33 -0700

    add user/session controllers

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,41 @@
+class Api::SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      sign_in(@user)
+      render "api/users/show"
+    else
+      @errors = ['invalid Username/Password']
+      render "api/shared/error", status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      sign_out
+      render "api/users/show"
+    else
+      @errors = ['no one logged in']
+      render "api/shared/error", status: 404
+    end
+  end
+
+  def show
+    if current_user
+      @user = current_user
+      render "api/users/show"
+    else
+      @errors = nil
+      render "api/shared/error", status: 404
+    end
+  end
+
+end
