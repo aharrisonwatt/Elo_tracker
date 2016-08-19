a12612e97c7971ab515b1d3d44eb752492356c7c
aharrisonwatt <aharrisonwatt@gmail.com>
2016-08-19 13:51:25 -0700

    switch database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160816214640) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.string   "name",       null: false
     t.datetime "created_at", null: false
