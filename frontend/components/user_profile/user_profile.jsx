91c69b8260270e1593c23d3fcb5dd57463daeaec
aharrisonwatt <aharrisonwatt@gmail.com>
2016-10-27 11:51:38 -0700

    root_reducer updated

--- frontend/components/user_profile/user_profile.jsx
+++ frontend/components/user_profile/user_profile.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
+import RatingGameFilter from '../rating_index/rating_game_filter';
 
 class UserProfile extends React.Component{
   constructor(props) {
     super(props);
+    this.state = {
+      gameFilter: 'Street Fighter V'
+    };
+    this.updateGameFilter = this.updateGameFilter.bind(this);
   }
 
   componentDidMount() {
     this.props.requestUser(this.props.params.username);
   }
 
+  updateGameFilter(game_name) {
+    this.state['gameFilter'] = game_name
+    this.forceUpdate();
+  }
+
   render() {
+    let games;
+    if (this.props.user['user']){
+      debugger
+    }
     return(
       <div>
-        <h1>Hello World</h1>
+        <div>
+          <h1>{this.props.params.username}</h1>
+          <RatingGameFilter
+            updateGameFilter={this.updateGameFilter}
+            games={games}/>
+        </div>
       </div>
     )
   }
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import RatingsReducer from './ratings_reducer';
+import UserReducer from './user_reducer';
 
 export default combineReducers({
+  user: UserReducer,
   ratings: RatingsReducer
 });
