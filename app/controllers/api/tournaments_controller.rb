f7d49fe7bfa2b6af3de14ae587edfb1aaab60af4
aharrisonwatt <aharrisonwatt@gmail.com>
2017-04-23 16:18:44 -0700

    formated tournaments API

--- app/controllers/api/tournaments_controller.rb
+++ app/controllers/api/tournaments_controller.rb
@@ -1,6 +1,6 @@
 class Api::TournamentsController < ApplicationController
   def index
-    @tournaments = Tournament.all
+    @tournaments = Tournament.generate_tournaments_object
     render 'index'
   end
 end
--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class Tournament < ApplicationRecord
   validates :name, :game_id, :bracket, :date, presence: true
 
@@ -12,5 +14,42 @@ class Tournament < ApplicationRecord
         bracket: bracket_url,
         date: date
       })
+  end
+
+  def self.generate_tournaments_object
+    #set object that is going to be returned
+    tournaments_array = []
+
+    #generate game_key hash for quicker lookup
+    game_key = {}
+    Game.all.each do |game|
+      game_key[game.id] = game.name
+    end
+
+    #iterate across all Tournaments to generate information
+    Tournament.all.each do |tournament|
+      #set tournament_object information that is needed
+      tournament_object = {
+        :name => tournament.name,
+        :game => game_key[tournament.game_id],
+        :date => tournament.date,
+        :bracket => tournament.bracket,
+        :placings => []
+      }
+      #add placement information
+      placings_array = []
+      tournament.placings.each do |placing|
+        user_name = User.find_by_id(placing.user_id).username
+        placings_array << [user_name, placing.placement]
+      end
+
+      placings_array.sort_by!{|_key, value| value}
+      tournament_object[:placings] = placings_array
+
+      #add tournament_object to tournaments_object
+      tournaments_array << tournament_object
+    end
+
+    return tournaments_array
   end
 end
