cd3858974b72375c1f16b540c1cea71570deded6
aharrisonwatt <aharrisonwatt@gmail.com>
2017-04-27 15:48:19 -0700

    add tournomant index list

---
+++ frontend/components/tournament_index/tournament_index_list.jsx
@@ -0,0 +1,14 @@
+import { hashHistory } from 'react-router';
+import React from 'react';
+
+class TournamentIndexList extends React.Componet {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let indexItems;
+
+    
+  }
+}
